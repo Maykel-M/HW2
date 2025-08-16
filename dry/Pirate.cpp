5678e3bcdf6bf2b0e156150a1cb613c3a0c19a43
Maykel-M <maykelmorozov@gmail.com>
2025-08-16 22:40:08 +0300

    3.3 bounty field add

--- dry/Pirate.cpp
+++ dry/Pirate.cpp
@@ -2,13 +2,20 @@
 #include "Pirate.h"
 
 
-Pirate::Pirate(const string& name): name(name) {}
+Pirate::Pirate(const string& name, unsigned int bounty): name(name), bounty(bounty) {}
 
 
 void Pirate::setName(const string& name){
     this->name = name;
 }
 
+void Pirate::setBounty(unsigned int bounty) {
+    this->bounty = bounty;
+}
+
+unsigned int Pirate::getBounty() {
+    return bounty;
+}
 
 std::string Pirate::getName(){
     return name;
@@ -16,6 +23,6 @@ std::string Pirate::getName(){
 
 
 std::ostream &operator<<(std::ostream &os, const Pirate &pirate){
-    os << pirate.name;
+    os << pirate.name << std::endl << pirate.bounty;
     return os;
 }
--- dry/Pirate.h
+++ dry/Pirate.h
@@ -8,16 +8,20 @@ using std::string;
 class Pirate {
 private:
     string name;
-
+    unsigned int bounty;
 public:
     Pirate() = default;
-    Pirate(const string& name);
+    Pirate(const string& name, unsigned int bounty);
 
     ~Pirate() = default;
 
     void setName(const string& name);
 
+    void setBounty(unsigned int bounty);
+
     string getName();
+
+    unsigned int getBounty();
 
     friend std::ostream &operator<<(std::ostream &os, const Pirate &pirate);
 };
